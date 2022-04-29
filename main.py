81cd1ddafd3d45620edce4c5e1c3c1c2c97dc8e8
0x2Hacks <junlvejiang@gmail.com>
2022-04-29 07:19:06 +0000

    add collection tats

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import openseatrader as ot
 
-meta = ot.retrieve_a_single_collection("crypto-coral-tribe")
-print(meta.json_data)
+meta = ot.retrieve_collection_stats('crypto-coral-tribe')
+print(meta)
--- openseatrader/wrapper.py
+++ openseatrader/wrapper.py
@@ -3,9 +3,17 @@ from openseatrader.models.collection import Collection
 
 _s = Session()
 
+def _send(request):
+    return _s.send(request.prepare()).json()
+
 def retrieve_a_single_collection(collection_slug):
     url = "https://api.opensea.io/api/v1/collection/{collection_slug}".format(collection_slug=collection_slug)
     r = Request('GET', url)
     collection = Collection(collection_slug)
-    collection.update(_s.send(r.prepare()).json()['collection'])
+    collection.update(_send(r)['collection'])
     return collection
+
+def retrieve_collection_stats(collection_slug):
+    url = "https://api.opensea.io/api/v1/collection/{collection_slug}/stats".format(collection_slug=collection_slug)
+    r = Request('GET', url)
+    return _send(r)
